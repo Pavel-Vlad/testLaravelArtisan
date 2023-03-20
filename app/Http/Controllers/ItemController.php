ec931bfda0de86addfe2ff4c789f619060e975de
Pavel Vladimirovich <sawel@yandex.ru>
2023-03-20 20:46:27 +0800

    create item model and store her in DB from api request

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $start = microtime(true);
+        $memory = memory_get_usage();
+
+        $item = Item::query()->create([
+            'data'=> $request->data,
+            'user_id' => $request->user()->id
+        ]);
+
+        $memory = memory_get_usage() - $memory;
+        $time = round(microtime(true) - $start, 3);
+
+        return response()->json([
+            'id' => $item->id,
+            'time' => $time,
+            'memory' => $memory
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
